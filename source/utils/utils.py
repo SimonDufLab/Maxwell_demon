d90d02cdc301e5f64134eefef4f85944b7c52350
SimonDufLab <Sduf.lab@gmail.com>
2022-05-05 12:00:44 -0400

    Include dataloader and removed shift variable (replace by random sampling)

--- source/utils/utils.py
+++ source/utils/utils.py
@@ -3,9 +3,11 @@ import jax
 import jax.numpy as jnp
 import numpy as np
 import optax
+import tensorflow_datasets as tfds
+import tensorflow as tf
 from jax.tree_util import Partial
 
-from typing import Any, Mapping, Tuple
+from typing import Any, Iterator, Mapping, Tuple, Union
 
 OptState = Any
 Batch = Mapping[int, np.ndarray]
@@ -22,18 +24,18 @@ def death_check_given_model(model):
 
 def accuracy_given_model(model):
     @jax.jit
-    def _accuracy(_params: hk.Params, _batch: Batch, _shift=0) -> jnp.ndarray:
+    def _accuracy(_params: hk.Params, _batch: Batch) -> jnp.ndarray:
         predictions = model.apply(_params, _batch)
-        return jnp.mean(jnp.argmax(predictions, axis=-1) == _batch[1]-_shift)
+        return jnp.mean(jnp.argmax(predictions, axis=-1) == _batch[1])
 
     return _accuracy
 
 
 def loss_given_model(model):
     @jax.jit
-    def loss(params: hk.Params, batch: Batch, shift=0) -> jnp.ndarray:
+    def loss(params: hk.Params, batch: Batch) -> jnp.ndarray:
         logits = model.apply(params, batch)
-        labels = jax.nn.one_hot(batch[1] - shift, 10)
+        labels = jax.nn.one_hot(batch[1], 10)
 
         # l2_loss = 0.5 * sum(jnp.sum(jnp.square(p)) for p in jax.tree_leaves(params))
         cdg_loss = 0.5 * sum(jnp.sum(jnp.power(jnp.clip(p, 0), 2)) for p in jax.tree_leaves(params))
@@ -49,11 +51,35 @@ def update_given_model_and_optimizer(model, optimizer):
     loss = loss_given_model(model)
 
     @jax.jit
-    def _update(_params: hk.Params, _opt_state: OptState, _batch: Batch,
-                _shift=0) -> Tuple[hk.Params, OptState]:
-        grads = jax.grad(loss)(_params, _batch, _shift)
+    def _update(_params: hk.Params, _opt_state: OptState, _batch: Batch) -> Tuple[hk.Params, OptState]:
+        grads = jax.grad(loss)(_params, _batch)
         updates, _opt_state = optimizer.update(grads, _opt_state)
         new_params = optax.apply_updates(_params, updates)
         return new_params, _opt_state
 
     return _update
+
+
+def load_mnist(
+    split: str,
+    *,
+    is_training: bool,
+    batch_size: int,
+    subset: Union[None, int] = None,
+) -> Iterator[Batch, None, None]:
+  """Loads the dataset as a generator of batches.
+    subset: If only want a subset, number of classes to build the subset from
+    """
+  ds = tfds.load("mnist:3.*.*", split=split, as_supervised=True).cache().repeat()
+  if subset:
+      assert subset < 10, "subset must be smaller than 10"
+      indices = np.random.choice(10, subset, replace=False)
+
+      def filter_fn(image, label):
+        return tf.reduce_any(indices == int(label))
+      ds = ds.filter(filter_fn)  # Only tae the randomly selected subset
+
+  if is_training:
+    ds = ds.shuffle(10 * batch_size, seed=0)
+  ds = ds.batch(batch_size)
+  return iter(tfds.as_numpy(ds))
