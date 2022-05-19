a35c4ab9f489d82ecf8f26e494ff04274cb0f44d
SimonDufLab <Sduf.lab@gmail.com>
2022-05-19 11:18:39 -0400

    Finished setting up aim logger for reinitialisation exp. Saving plt figs separately for better rendering than aim.

--- source/reinitialisation_exp.py
+++ source/reinitialisation_exp.py
@@ -6,6 +6,8 @@ import jax.numpy as jnp
 import numpy as np
 import matplotlib.pyplot as plt
 from aim import Run, Figure
+import os
+import time
 
 from models.mlp import lenet_var_size
 import utils.utils as utl
@@ -21,11 +23,11 @@ if __name__ == "__main__":
         "total_steps": 2001,
         "report_freq": 50,
         "record_freq": 10,
-        "switching_period": 1000,  # Switch dataset periodically
+        "switching_period": 500,  # Switch dataset periodically
         "reset_period": 250,  # After reset_period steps, reinitialize the parameters
         "reset_horizon": 1.0,  # Set to lower than one if you want to stop resetting before final steps
         "kept_classes": 3,  # Number of classes in the randomly selected subset
-        "compare_full_reset": True,  # Additional training where a complete reset of the parameters is done (vs a partial)
+        "compare_full_reset": True,  # Include the comparison with a complete reset of the parameters
         "lr": 1e-3,
         "optimizer": "adam",
         "dataset": "mnist",
@@ -33,13 +35,16 @@ if __name__ == "__main__":
         "reg_param": 1e-4,
     }
 
+    total_neurons = exp_config["size"] + 3 * exp_config["size"]
+
     assert exp_config["optimizer"] in optimizer_choice.keys(), "Currently supported optimizers: " + str(
         optimizer_choice.keys())
     assert exp_config["dataset"] in dataset_choice.keys(), "Currently supported datasets: " + str(dataset_choice.keys())
     assert exp_config["regularizer"] in regularizer_choice, "Currently supported datasets: " + str(regularizer_choice)
 
     # Logger config
-    exp_run = Run(repo="./logs", experiment="reinitialisation experiment")
+    exp_name = "reinitialisation_experiment"
+    exp_run = Run(repo="./logs", experiment=exp_name)
     exp_run["configuration"] = exp_config
 
     # Load the dataset
@@ -127,30 +132,36 @@ if __name__ == "__main__":
             test_accuracy_partial_reinit = accuracy_fn(params_partial_reinit, test_batch)
             no_reinit_perf.append(test_accuracy)
             exp_run.track(jax.device_get(no_reinit_perf[-1]),
-                          name="Test accuracy, no reinitialisation", step=step)
+                          name="Test accuracy", step=step, context={"reinitialisation": 'None'})
             partial_reinit_perf.append(test_accuracy_partial_reinit)
             exp_run.track(jax.device_get(partial_reinit_perf[-1]),
-                          name="Test accuracy, partial reinitialisation", step=step)
+                          name="Test accuracy", step=step,  context={"reinitialisation": 'Partial'})
 
-            # Record dean neurons
+            # Record dead neurons
             death_test_batch = next(test_death)
             no_reinit_dead_neurons.append(utl.count_dead_neurons(
                 utl.death_check_given_model(net)(params, death_test_batch)))
             exp_run.track(jax.device_get(no_reinit_dead_neurons[-1]),
-                          name="Dead neurons, no reinitialisation", step=step)
+                          name="Dead neurons", step=step, context={"reinitialisation": 'None'})
+            exp_run.track(jax.device_get(no_reinit_dead_neurons[-1]/total_neurons),
+                          name="Dead neurons ratio", step=step, context={"reinitialisation": 'None'})
             partial_reinit_dead_neurons.append(utl.count_dead_neurons(
                 utl.death_check_given_model(net)(params_partial_reinit, death_test_batch)))
             exp_run.track(jax.device_get(partial_reinit_dead_neurons[-1]),
-                          name="Dead neurons, partial reinitialisation", step=step)
+                          name="Dead neurons", step=step, context={"reinitialisation": 'Partial'})
+            exp_run.track(jax.device_get(partial_reinit_dead_neurons[-1]/total_neurons),
+                          name="Dead neurons ratio", step=step, context={"reinitialisation": 'Partial'})
             if exp_config["compare_full_reset"]:
                 test_accuracy_hard_reinit = jax.device_get(accuracy_fn(params_hard_reinit, test_batch))
                 hard_reinit_perf.append(test_accuracy_hard_reinit)
                 exp_run.track(jax.device_get(hard_reinit_perf[-1]),
-                              name="Test accuracy, complete reinitialisation", step=step)
+                              name="Test accuracy", step=step, context={"reinitialisation": 'Complete'})
                 hard_reinit_dead_neurons.append(utl.count_dead_neurons(
                     utl.death_check_given_model(net)(params_hard_reinit, death_test_batch)))
                 exp_run.track(jax.device_get(hard_reinit_dead_neurons[-1]),
-                              name="Dead neurons, complete reinitialisation", step=step)
+                              name="Dead neurons", step=step, context={"reinitialisation": 'Complete'})
+                exp_run.track(jax.device_get(hard_reinit_dead_neurons[-1]/total_neurons),
+                              name="Dead neurons ratio", step=step, context={"reinitialisation": 'Complete'})
 
         # Training step
         train_batch = next(train)
@@ -162,25 +173,27 @@ if __name__ == "__main__":
                                                                   opt_hard_reinit_state, train_batch)
 
     # Plots
-    total_neurons = exp_config["size"] + 3 * exp_config["size"]
+    dir_path = "./logs/plots/"+exp_name+time.strftime("/%Y-%m-%d---%B %d---%H:%M:%S/")
+    os.makedirs(dir_path)
     x = list(range(0, exp_config["total_steps"], exp_config["record_freq"]))
     fig1 = plt.figure(figsize=(15, 10))
-    plt.plot(x, no_reinit_perf, color='red', linewidth=1, label="accuracy, no reinitialisation")
-    plt.plot(x, partial_reinit_perf, color='green', linewidth=1, label="accuracy, with partial reinitialisation")
+    plt.plot(x, no_reinit_perf, color='red', label="accuracy, no reinitialisation")
+    plt.plot(x, partial_reinit_perf, color='green', label="accuracy, with partial reinitialisation")
     plt.plot(x, np.array(no_reinit_dead_neurons) / total_neurons, color='red', linewidth=3, linestyle=':',
              label="dead neurons, no reinitialisation")
     plt.plot(x, np.array(partial_reinit_dead_neurons) / total_neurons, color='green', linewidth=3, linestyle=':',
              label="dead neurons, with partial reinitialisation")
     if exp_config["compare_full_reset"]:
-        plt.plot(x, hard_reinit_perf, color='blue', linewidth=1, label="accuracy, complete reset")
-        plt.plot(x, np.array(hard_reinit_dead_neurons) / total_neurons, color='blue', linewidth=3, linestyle=':',
+        plt.plot(x, hard_reinit_perf, color='cyan', label="accuracy, complete reset")
+        plt.plot(x, np.array(hard_reinit_dead_neurons) / total_neurons, color='cyan', linewidth=3, linestyle=':',
                  label="dead neurons, complete reset")
 
     plt.xlabel("Iterations", fontsize=16)
     plt.ylabel("Inactive neurons/accuracy", fontsize=16)
-    plt.title(f"Relevant title, {exp_config['kept_classes']} classes", fontweight='bold', fontsize=20)
-    plt.legend(prop={'size': 16})
-    # plt.show()
+    plt.title(f"Performance vs dead neurons, switching between {exp_config['kept_classes']}"
+              f" classes on {exp_config['dataset']}", fontweight='bold', fontsize=20)
+    plt.legend(prop={'size': 12})
+    fig1.savefig(dir_path+"perf_vs_dead_neurons.png")
     aim_fig1 = Figure(fig1)
     exp_run.track(aim_fig1, name="Switching task performance w/r to dead neurons", step=0)
 
@@ -189,8 +202,10 @@ if __name__ == "__main__":
     plt.plot(partial_reinit_dead_neurons, label="partial reinitialisation")
     if exp_config["compare_full_reset"]:
         plt.plot(hard_reinit_dead_neurons, label="complete reinitialisation")
-    plt.title("Dead neurons")
-    plt.legend()
-    # plt.show()
+    plt.title("Dead neurons during training", fontweight='bold', fontsize=20)
+    plt.xlabel("Iterations", fontsize=16)
+    plt.ylabel("Inactive neurons", fontsize=16)
+    plt.legend(prop={'size': 12})
+    fig2.savefig(dir_path+"dead_neurons.png")
     aim_fig2 = Figure(fig2)
     exp_run.track(aim_fig2, name="Dead neurons over training time", step=0)
