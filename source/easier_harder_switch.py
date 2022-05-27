86c3cf8cf3063c34260fdbe213f6c791b1c0fb0a
SimonDufLab <Sduf.lab@gmail.com>
2022-05-27 16:13:05 -0400

    Setted up hydra on one experiment. Working

--- source/easier_harder_switch.py
+++ source/easier_harder_switch.py
@@ -8,6 +8,11 @@ import matplotlib.pyplot as plt
 from aim import Run, Figure
 import os
 import time
+from dataclasses import dataclass, asdict
+from typing import Tuple, Union
+import hydra
+from hydra.core.config_store import ConfigStore
+from omegaconf import OmegaConf
 
 from models.mlp import lenet_var_size
 import utils.utils as utl
@@ -15,42 +20,64 @@ from utils.utils import build_models
 from utils.config import dataset_choice, optimizer_choice, regularizer_choice
 from copy import deepcopy
 
-if __name__ == "__main__":
+
+# Configuration
+@dataclass
+class ExpConfig:
+    size: int = 100  # Number of hidden units in first layer; size*3 in second hidden layer
+    total_steps: int = 1001
+    report_freq: int = 50
+    record_freq: int = 10
+    lr: float = 1e-3
+    optimizer: str = "adam"
+    datasets: Tuple[str] = ("mnist", "fashion mnist")  # Datasets to use, listed from easier to harder
+    kept_classes: Tuple[Union[int, None]] = (None, None)  # Number of classes to use, listed from easier to harder
+    regularizer: str = "cdg_l2"
+    reg_param: float = 1e-4
+
+
+cs = ConfigStore.instance()
+# Registering the Config class with the name '_config'.
+cs.store(name="_config", node=ExpConfig)
+
+
+@hydra.main(version_base=None, config_name="_config")
+def run_exp(exp_config: ExpConfig) -> None:
 
     # Configuration
-    exp_config = {
-        "size": 100,  # Number of hidden units in first layer; size*3 in second hidden layer
-        "total_steps": 1001,
-        "report_freq": 50,
-        "record_freq": 10,
-        "lr": 1e-3,
-        "optimizer": "adam",
-        "datasets": ("mnist", "fashion mnist"),  # Datasets to use, listed from easier to harder
-        "kept_classes": (None, None),  # Number of classes to use, listed from easier to harder
-        "regularizer": "cdg_l2",
-        "reg_param": 1e-4,
-    }
+    # exp_config = {
+    #     "size": 100,  # Number of hidden units in first layer; size*3 in second hidden layer
+    #     "total_steps": 1001,
+    #     "report_freq": 50,
+    #     "record_freq": 10,
+    #     "lr": 1e-3,
+    #     "optimizer": "adam",
+    #     "datasets": ("mnist", "fashion mnist"),  # Datasets to use, listed from easier to harder
+    #     "kept_classes": (None, None),  # Number of classes to use, listed from easier to harder
+    #     "regularizer": "cdg_l2",
+    #     "reg_param": 1e-4,
+    # }
 
-    total_neurons = exp_config["size"] + 3 * exp_config["size"]
+    total_neurons = exp_config.size + 3 * exp_config.size
     dataset_total_classes = 10  # TODO: allow compatibility with dataset > 10 classes
 
-    assert exp_config["optimizer"] in optimizer_choice.keys(), "Currently supported optimizers: " + str(
+    assert exp_config.optimizer in optimizer_choice.keys(), "Currently supported optimizers: " + str(
         optimizer_choice.keys())
-    assert exp_config["datasets"][0] in dataset_choice.keys(), "Currently supported datasets: " + str(
+    assert exp_config.datasets[0] in dataset_choice.keys(), "Currently supported datasets: " + str(
         dataset_choice.keys())
-    assert exp_config["datasets"][1] in dataset_choice.keys(), "Currently supported datasets: " + str(
+    assert exp_config.datasets[1] in dataset_choice.keys(), "Currently supported datasets: " + str(
         dataset_choice.keys())
-    assert exp_config["regularizer"] in regularizer_choice, "Currently supported datasets: " + str(regularizer_choice)
+    assert exp_config.regularizer in regularizer_choice, "Currently supported datasets: " + str(regularizer_choice)
 
     # Logger config
     exp_name = "easier_harder_switch_experiment"
     exp_run = Run(repo="./logs", experiment=exp_name)
-    exp_run["configuration"] = exp_config
+    exp_run["configuration"] = OmegaConf.to_container(exp_config)
 
     # Load the dataset for the 2 tasks (ez and hard)
-    load_data_easier = dataset_choice[exp_config["datasets"][0]]
-    if exp_config["kept_classes"][0]:
-        indices = np.random.choice(10, exp_config["kept_classes"][0], replace=False)
+    load_data_easier = dataset_choice[exp_config.datasets[0]]
+    if exp_config.kept_classes[0]:
+        indices = np.random.choice(10, exp_config.kept_classes[0], replace=False)
     else:
         indices = None
     train_easier = load_data_easier(split='train', is_training=True, batch_size=50, subset=indices, transform=False)
@@ -58,9 +85,9 @@ if __name__ == "__main__":
     test_eval_easier = load_data_easier(split='test', is_training=False, batch_size=250, subset=indices, transform=False)
     test_death_easier = load_data_easier(split='train', is_training=False, batch_size=1000, subset=indices, transform=False)
 
-    load_data_harder = dataset_choice[exp_config["datasets"][1]]
-    if exp_config["kept_classes"][1]:
-        indices = np.random.choice(10, exp_config["kept_classes"][1], replace=False)
+    load_data_harder = dataset_choice[exp_config.datasets[1]]
+    if exp_config.kept_classes[1]:
+        indices = np.random.choice(10, exp_config.kept_classes[1], replace=False)
     else:
         indices = None
     train_harder = load_data_harder(split='train', is_training=True, batch_size=50, subset=indices, transform=False)
@@ -74,12 +101,12 @@ if __name__ == "__main__":
     test_death = [test_death_easier, test_death_harder]
 
     # Create network/optimizer and initialize params
-    architecture = lenet_var_size(exp_config["size"], dataset_total_classes)
+    architecture = lenet_var_size(exp_config.size, dataset_total_classes)
     net = build_models(architecture)
-    opt = optimizer_choice[exp_config["optimizer"]](exp_config["lr"])
+    opt = optimizer_choice[exp_config.optimizer](exp_config.lr)
 
     # Set training/monitoring functions
-    loss = utl.ce_loss_given_model(net, regularizer=exp_config["regularizer"], reg_param=exp_config["reg_param"],
+    loss = utl.ce_loss_given_model(net, regularizer=exp_config.regularizer, reg_param=exp_config.reg_param,
                                    classes=dataset_total_classes)
     accuracy_fn = utl.accuracy_given_model(net)
     update_fn = utl.update_given_loss_and_optimizer(loss, opt)
@@ -105,8 +132,8 @@ if __name__ == "__main__":
         partial_reinit_dead_neurons = []
         idx = order
 
-        for step in range(exp_config["total_steps"]):
-            if (step+1) / (exp_config["total_steps"]//2) == 1:  # Switch task at mid-training
+        for step in range(exp_config.total_steps):
+            if (step+1) / (exp_config.total_steps//2) == 1:  # Switch task at mid-training
                 # reinitialize dead neurons
                 _, key = jax.random.split(key)
                 new_params = net.init(key, next(train[idx]))
@@ -117,7 +144,7 @@ if __name__ == "__main__":
                 # switch task
                 idx += -1
 
-            if step % exp_config["report_freq"] == 0:
+            if step % exp_config.report_freq == 0:
                 # Periodically evaluate classification accuracy on train & test sets.
                 train_loss = loss(params, next(train_eval[idx]))
                 train_accuracy = accuracy_fn(params, next(train_eval[idx]))
@@ -126,7 +153,7 @@ if __name__ == "__main__":
                 print(f"[Step {step}] Train / Test accuracy: {train_accuracy:.3f} / "
                       f"{test_accuracy:.3f}. Loss: {train_loss:.3f}.")
 
-            if step % exp_config["record_freq"] == 0:
+            if step % exp_config.record_freq == 0:
                 # Record accuracies
                 test_batch = next(test_eval[idx])
                 test_accuracy = accuracy_fn(params, test_batch)
@@ -166,7 +193,7 @@ if __name__ == "__main__":
                                                                         opt_partial_reinit_state, train_batch)
 
         # Plots
-        x = list(range(0, exp_config["total_steps"], exp_config["record_freq"]))
+        x = list(range(0, exp_config.total_steps, exp_config.record_freq))
         fig = plt.figure(figsize=(25, 15))
         plt.plot(x, no_reinit_perf, color='red', label="accuracy, no reinitialisation")
         plt.plot(x, partial_reinit_perf, color='green', label="accuracy, with partial reinitialisation")
@@ -177,10 +204,14 @@ if __name__ == "__main__":
         plt.xlabel("Iterations", fontsize=16)
         plt.ylabel("Inactive neurons (ratio)/accuracy", fontsize=16)
         plt.title(f"From {setting[order]} switch experiment"
-                  f" on {exp_config['datasets'][0]}/{exp_config['datasets'][1]}"
-                  f", keeping {exp_config['kept_classes'][0]}/{exp_config['kept_classes'][1]} classes",
+                  f" on {exp_config.datasets[0]}/{exp_config.datasets[1]}"
+                  f", keeping {exp_config.kept_classes[0]}/{exp_config.kept_classes[1]} classes",
                   fontweight='bold', fontsize=20)
         plt.legend(prop={'size': 12})
         fig.savefig(dir_path + f"{setting[order]}.png")
         aim_fig = Figure(fig)
         exp_run.track(aim_fig, name=f"From {setting[order]} experiment", step=0)
+
+
+if __name__ == "__main__":
+    run_exp()
