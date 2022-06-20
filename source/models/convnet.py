7cb21a3c0f626b9460e2a46a115c7cce35dd3c13
SimonDufLab <Sduf.lab@gmail.com>
2022-06-20 12:09:47 -0400

    Added conv_4_2 architecture; modified conv_6_2; paddind mnist like dataset for more easily reusing same architecture on the different datasets

--- source/models/convnet.py
+++ source/models/convnet.py
@@ -5,8 +5,7 @@ from jax.tree_util import Partial
 
 
 def conv_3_2(sizes, number_classes, dim=2):
-    """ Build a MLP with 2 hidden layers similar to popular LeNet, but with varying number of hidden units
-    sizes: Tuple containing output_channel of first conv layer and number of features in first fully connected
+    """ Convnet with 3 convolutional layers followed by 2 fully-connected
     """
     def act():
         return jax.nn.relu
@@ -28,9 +27,32 @@ def conv_3_2(sizes, number_classes, dim=2):
     return [layer_1, layer_2, layer_3, layer_4, layer_5]
 
 
+def conv_4_2(sizes, number_classes, dim=2):
+    """ Convnet with 4 convolutional layers followed by 2 fully-connected
+    """
+    def act():
+        return jax.nn.relu
+    if dim==1:
+        conv_fn = hk.Conv1D
+    if dim==2:
+        conv_fn = hk.Conv2D
+    else:
+        raise Exception("Convnet dimension restricted to 1 or 2")
+    max_pool = Partial(hk.MaxPool, window_shape=(2, 2), strides=2, padding="VALID")
+    bigger_max_pool = Partial(hk.MaxPool, window_shape=(4, 4), strides=4, padding="VALID")
+
+    layer_1 = [Partial(conv_fn, sizes[0], 3), act]
+    layer_2 = [max_pool, Partial(conv_fn, 2*sizes[0], 3), act]
+    layer_3 = [max_pool, Partial(conv_fn, 4*sizes[0], 3), act]
+    layer_4 = [max_pool, Partial(conv_fn, 4 * sizes[0], 3), act]
+    layer_5 = [bigger_max_pool, hk.Flatten, Partial(hk.Linear, sizes[1]), act]
+    layer_6 = [Partial(hk.Linear, number_classes)]
+
+    return [layer_1, layer_2, layer_3, layer_4, layer_5, layer_6]
+
+
 def conv_6_2(sizes, number_classes, dim=2):
-    """ Build a MLP with 2 hidden layers similar to popular LeNet, but with varying number of hidden units
-    sizes: Tuple containing output_channel of first conv layer and number of features in first fully connected
+    """ Convnet with 6 convolutional layers followed by 2 fully-connected
     """
     def act():
         return jax.nn.relu
@@ -48,7 +70,7 @@ def conv_6_2(sizes, number_classes, dim=2):
     layer_3 = [max_pool, Partial(conv_fn, 2*sizes[0], 3), act]
     layer_4 = [Partial(conv_fn, 2*sizes[0], 3), act]
     layer_5 = [max_pool, Partial(conv_fn, 4*sizes[0], 3), act]
-    layer_6 = [Partial(conv_fn, 4*sizes[0], 3), act]
+    layer_6 = [max_pool, Partial(conv_fn, 4*sizes[0], 3), act]
     layer_7 = [bigger_max_pool, hk.Flatten, Partial(hk.Linear, sizes[1]), act]
     layer_8 = [Partial(hk.Linear, number_classes)]
 
