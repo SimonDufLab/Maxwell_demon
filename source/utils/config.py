6f45ea3480e666a7898b08130c3b589ece80b228
SimonDufLab <Sduf.lab@gmail.com>
2022-07-19 15:40:53 -0400

    Added cifar100 to datasets

--- source/utils/config.py
+++ source/utils/config.py
@@ -1,6 +1,6 @@
 """Optax optimizers configured to work with logger"""
 import optax
-from utils.utils import load_mnist_torch, load_cifar10_torch, load_fashion_mnist_torch
+from utils.utils import load_mnist_torch, load_cifar10_torch, load_fashion_mnist_torch, load_cifar100_tf
 from utils.utils import load_mnist_tf, load_cifar10_tf, load_fashion_mnist_tf
 from models.mlp import mlp_3
 from models.convnet import conv_3_2, conv_4_2, conv_6_2
@@ -18,6 +18,7 @@ dataset_choice = {
     "fashion mnist-torch": load_fashion_mnist_torch,
     "cifar10": load_cifar10_tf,
     "cifar10-torch": load_cifar10_torch,
+    "cifar100": load_cifar100_tf,
 }
 
 regularizer_choice = (
