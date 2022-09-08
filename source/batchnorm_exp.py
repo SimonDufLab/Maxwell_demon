8672f4936d45900732009b2553a8bb37f3692166
SimonDufLab <Sduf.lab@gmail.com>
2022-09-08 13:37:18 -0400

    WIP: Added BN models and finished the backbone of the experiment (runnable). Need to add logs for distance to border distribution and to test

--- source/batchnorm_exp.py
+++ source/batchnorm_exp.py
@@ -25,7 +25,7 @@ from jax.flatten_util import ravel_pytree
 
 import utils.utils as utl
 from utils.utils import build_models
-from utils.config import optimizer_choice, dataset_choice, dataset_target_cardinality, regularizer_choice, architecture_choice
+from utils.config import optimizer_choice, dataset_choice, dataset_target_cardinality, regularizer_choice, architecture_choice, bn_architecture_choice
 
 
 # Experience name -> for aim logger
@@ -74,7 +74,7 @@ def run_exp(exp_config: ExpConfig) -> None:
     assert exp_config.optimizer in optimizer_choice.keys(), "Currently supported optimizers: " + str(optimizer_choice.keys())
     assert exp_config.dataset in dataset_choice.keys(), "Currently supported datasets: " + str(dataset_choice.keys())
     assert exp_config.regularizer in regularizer_choice, "Currently supported regularizers: " + str(regularizer_choice)
-    assert exp_config.architecture in architecture_choice.keys(), "Current architectures available: " + str(architecture_choice.keys())
+    assert exp_config.architecture in bn_architecture_choice.keys(), "Current architectures available: " + str(bn_architecture_choice.keys())
 
     if exp_config.regularizer == 'None':
         exp_config.regularizer = None
@@ -103,3 +103,95 @@ def run_exp(exp_config: ExpConfig) -> None:
     test_size, test_eval = load_data(split="test", is_training=False, batch_size=eval_size, cardinality=True)
     dataset_size, test_death = load_data(split="train", is_training=False,
                                          batch_size=exp_config.death_batch_size, cardinality=True)
+
+    for arc_choice, context in ((architecture_choice, bn_architecture_choice), ('With BN', 'Without BN')):
+        # Time the subrun, with or without BN
+        subrun_start_time = time.time()
+
+        # Make the network and optimiser
+        architecture = arc_choice[exp_config.architecture]
+        classes = dataset_target_cardinality[exp_config.dataset]  # Retrieving the number of classes in dataset
+        architecture = architecture(exp_config.size, classes)
+        net = build_models(*architecture)
+        opt = optimizer_choice[exp_config.optimizer](exp_config.lr)
+
+        # Set training/monitoring functions
+        loss = utl.ce_loss_given_model(net, regularizer=exp_config.regularizer, reg_param=exp_config.reg_param)
+        test_loss = utl.ce_loss_given_model(net, regularizer=exp_config.regularizer, reg_param=exp_config.reg_param,
+                                            is_training=False)
+        accuracy_fn = utl.accuracy_given_model(net)
+        update_fn = utl.update_given_loss_and_optimizer(loss, opt, exp_config.add_noise, exp_config.noise_imp)
+        death_check_fn = utl.death_check_given_model(net)
+        scan_len = dataset_size // exp_config.death_batch_size
+        scan_death_check_fn = utl.scanned_death_check_fn(death_check_fn, scan_len)
+        scan_death_check_fn_with_activations_data = utl.scanned_death_check_fn(
+            utl.death_check_given_model(net, with_activations=True), scan_len, True)
+        final_accuracy_fn = utl.create_full_accuracy_fn(accuracy_fn, test_size // eval_size)
+        full_train_acc_fn = utl.create_full_accuracy_fn(accuracy_fn, train_size // eval_size)
+
+        params, state = net.init(jax.random.PRNGKey(exp_config.init_seed), next(train))
+        opt_state = opt.init(params)
+
+        starting_neurons, starting_per_layer = utl.get_total_neurons(exp_config.architecture, exp_config.size)
+        total_neurons, total_per_layer = starting_neurons, starting_per_layer
+
+        for step in range(exp_config.training_steps):
+            if step % exp_config.record_freq == 0:
+                train_loss = test_loss(params, state, next(train_eval))
+                train_accuracy = accuracy_fn(params, state, next(train_eval))
+                test_accuracy = accuracy_fn(params, state, next(test_eval))
+                train_accuracy, test_accuracy = jax.device_get((train_accuracy, test_accuracy))
+                # Periodically print classification accuracy on train & test sets.
+                if step % exp_config.report_freq == 0:
+                    print(f"[Step {step}] Train / Test accuracy: {train_accuracy:.3f} / "
+                          f"{test_accuracy:.3f}. Loss: {train_loss:.3f}.")
+                dead_neurons = death_check_fn(params, state, next(test_death))
+                # Record some metrics
+                dead_neurons_count, _ = utl.count_dead_neurons(dead_neurons)
+                exp_run.track(jax.device_get(dead_neurons_count), name="Dead neurons", step=step,
+                              context={"Normalization": context})
+                exp_run.track(jax.device_get(total_neurons - dead_neurons_count), name="Live neurons", step=step,
+                              context={"Normalization": context})
+                exp_run.track(test_accuracy, name="Test accuracy", step=step,
+                              context={"Normalization": context})
+                exp_run.track(train_accuracy, name="Train accuracy", step=step,
+                              context={"Normalization": context})
+                exp_run.track(jax.device_get(train_loss), name="Train loss", step=step,
+                              context={"Normalization": context})
+
+            params, state, opt_state = update_fn(params, state, opt_state, next(train))
+
+        activations_data, final_dead_neurons = scan_death_check_fn_with_activations_data(params, state, test_death)
+
+        activations_max, activations_mean, activations_count = activations_data
+        activations_max, _ = ravel_pytree(activations_max)
+        activations_max = jax.device_get(activations_max)
+        activations_mean, _ = ravel_pytree(activations_mean)
+        activations_mean = jax.device_get(activations_mean)
+        activations_count, _ = ravel_pytree(activations_count)
+        activations_count = jax.device_get(activations_count)
+
+        activations_max_dist = Distribution(activations_max, bin_count=100)
+        exp_run.track(activations_max_dist, name='Maximum activation distribution after convergence', step=0,
+                      context={"Normalization": context})
+        activations_mean_dist = Distribution(activations_mean, bin_count=100)
+        exp_run.track(activations_mean_dist, name='Mean activation distribution after convergence', step=0,
+                      context={"Normalization": context})
+        activations_count_dist = Distribution(activations_count, bin_count=50)
+        exp_run.track(activations_count_dist, name='Activation count per neuron after convergence', step=0,
+                      context={"Normalization": context})
+
+        # Print running time
+        print()
+        print(f"Running time for {context}: " + str(timedelta(seconds=time.time() - subrun_start_time)))
+        print("----------------------------------------------")
+        print()
+
+    # Print total runtime
+    print()
+    print("==================================")
+    print("Whole experiment completed in: " + str(timedelta(seconds=time.time() - run_start_time)))
+
+
+if __name__ == "__main__":
+    run_exp()
