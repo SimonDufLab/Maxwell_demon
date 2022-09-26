640424bb68a0bbd9c5f394d7b0668f6770879637
SimonDufLab <Sduf.lab@gmail.com>
2022-09-26 11:12:12 -0400

    Can modify activation fn in switch exps

--- source/models/resnet.py
+++ source/models/resnet.py
@@ -3,7 +3,8 @@ import haiku as hk
 import jax
 import jax.numpy as jnp
 from jax.tree_util import Partial
-from typing import Any, Mapping, Optional, Sequence, Union
+from jax.nn import relu
+from typing import Any, Callable, Mapping, Optional, Sequence, Union
 
 from haiku.nets import ResNet
 from haiku._src.nets.resnet import check_length
@@ -63,14 +64,16 @@ class ResnetBlock(ResNet.BlockV1):
             self,
             channels: int,
             stride: Union[int, Sequence[int]],
+            activation_fn: Callable,
             use_projection: bool,
             bottleneck: bool,
             is_training: bool,
             name: Optional[str] = None,
     ):
-        self.is_training = is_training
         super().__init__(channels=channels, stride=stride, use_projection=use_projection, bn_config=bn_config,
                          bottleneck=bottleneck, name=name)
+        self.is_training = is_training
+        self.activation_fn = activation_fn
 
     def __call__(self, inputs):
         out = shortcut = inputs
@@ -83,11 +86,11 @@ class ResnetBlock(ResNet.BlockV1):
         for i, (conv_i, bn_i) in enumerate(self.layers):
             out = conv_i(out)
             out = bn_i(out, self.is_training)
-            if i < len(self.layers) - 1:  # Don't apply relu on last layer
-                out = jax.nn.relu(out)
+            if i < len(self.layers) - 1:  # Don't apply act right away on last layer
+                out = self.activation_fn(out)
                 activations.append(out)
 
-        out = jax.nn.relu(out + shortcut)
+        out = self.activation_fn(out + shortcut)
         activations.append(out)
 
         return out, activations
@@ -98,6 +101,7 @@ class ResnetInit(hk.Module):
     def __init__(
             self,
             is_training: bool,
+            activation_fn: Callable,
             conv_config: Optional[Mapping[str, FloatStrOrBool]],
             bn_config: Optional[Mapping[str, FloatStrOrBool]],
             name: Optional[str] = None):
@@ -105,14 +109,15 @@ class ResnetInit(hk.Module):
         self.is_training = is_training
         self.bn = hk.BatchNorm(name="init_bn", **bn_config)
         self.conv = hk.Conv2D(**conv_config)
+        self.activation_fn = activation_fn
 
     def __call__(self, inputs):
         x = self.conv(inputs)
         x = self.bn(x, is_training=self.is_training)
-        return jax.nn.relu(x)
+        return self.activation_fn(x)
 
 
-def block_group(channels: int, num_blocks: int, stride: Union[int, Sequence[int]], bottleneck: bool,
+def block_group(channels: int, num_blocks: int, stride: Union[int, Sequence[int]], activation_fn: Callable, bottleneck: bool,
                 use_projection: bool):
     """Adapted from: https://github.com/deepmind/dm-haiku/blob/d6e3c2085253735c3179018be495ebabf1e6b17c/
     haiku/_src/nets/resnet.py#L200"""
@@ -124,6 +129,7 @@ def block_group(channels: int, num_blocks: int, stride: Union[int, Sequence[int]
         train_layers.append(
             [Partial(ResnetBlock, channels=channels,
                      stride=(1 if i else stride),
+                     activation_fn=activation_fn,
                      use_projection=(i == 0 and use_projection),
                      bottleneck=bottleneck,
                      is_training=True,)])
@@ -131,6 +137,7 @@ def block_group(channels: int, num_blocks: int, stride: Union[int, Sequence[int]
         test_layers.append(
             [Partial(ResnetBlock, channels=channels,
                      stride=(1 if i else stride),
+                     activation_fn=activation_fn,
                      use_projection=(i == 0 and use_projection),
                      bottleneck=bottleneck,
                      is_training=False,)])
@@ -141,6 +148,7 @@ def block_group(channels: int, num_blocks: int, stride: Union[int, Sequence[int]
 
 def resnet_model(blocks_per_group: Sequence[int],
                  num_classes: int,
+                 activation_fn: Callable,
                  bottleneck: bool = True,
                  channels_per_group: Sequence[int] = (256, 512, 1024, 2048),
                  use_projection: Sequence[bool] = (True, True, True, True),
@@ -156,8 +164,8 @@ def resnet_model(blocks_per_group: Sequence[int],
     # def act():
     #     return jax.nn.relu
 
-    train_layers = [[Partial(ResnetInit, is_training=True, conv_config=initial_conv_config, bn_config=bn_config)]]
-    test_layers = [[Partial(ResnetInit, is_training=False, conv_config=initial_conv_config, bn_config=bn_config)]]
+    train_layers = [[Partial(ResnetInit, is_training=True, activation_fn=activation_fn, conv_config=initial_conv_config, bn_config=bn_config)]]
+    test_layers = [[Partial(ResnetInit, is_training=False, activation_fn=activation_fn, conv_config=initial_conv_config, bn_config=bn_config)]]
 
     # train_layers.append([Partial(hk.Conv2D, **initial_conv_config),
     #                      Partial(Partial(hk.BatchNorm, name="initial_batchnorm", **bn_config), is_training=True), act])
@@ -168,6 +176,7 @@ def resnet_model(blocks_per_group: Sequence[int],
         block_train_layers, block_test_layers = block_group(channels=channels_per_group[i],
                                                             num_blocks=blocks_per_group[i],
                                                             stride=stride,
+                                                            activation_fn=activation_fn,
                                                             bottleneck=bottleneck,
                                                             use_projection=use_projection[i])
         if i == 0:
@@ -199,6 +208,7 @@ default_initial_conv_config = {"kernel_shape": 7,
 
 def resnet18(size: Union[int, Sequence[int]],
              num_classes: int,
+             activation_fn: Callable = relu,
              logits_config: Optional[Mapping[str, Any]] = default_logits_config,
              initial_conv_config: Optional[Mapping[str, FloatStrOrBool]] = default_initial_conv_config,
              strides: Sequence[int] = (1, 2, 2, 2)):
@@ -212,6 +222,7 @@ def resnet18(size: Union[int, Sequence[int]],
     default_initial_conv_config["output_channels"] = size
 
     return resnet_model(num_classes=num_classes,
+                        activation_fn=activation_fn,
                         initial_conv_config=initial_conv_config,
                         strides=strides,
                         logits_config=logits_config,
